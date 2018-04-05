f46ab9348c1d86003556f774128355c0df2933e2
Remy Brin-Fortier <remy_b-f@hotmail.com>
2018-04-04 20:07:06 -0400

    première version test unitaire longueurMinimum (Fin TP 9)

--- src/app/Shared/caracteres-validator.ts
+++ src/app/Shared/caracteres-validator.ts
@@ -10,4 +10,13 @@ export class VerifierCaracteresValidator {
             
         };
     }
+    static longueurMinimum(min: number): ValidatorFn {
+        return (c: AbstractControl): { [key: string]: boolean } | null => {
+            if (c.value.trim().length >= min) {
+                return { 'longueurMinimum': true };
+            }
+            return { 'longueurMinimum': false };
+            
+        };
+    }
 }
